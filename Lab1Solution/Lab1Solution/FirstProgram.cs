8784a0434727e815586c1394ebf405ff1f7f7e95
Vikram Kamalakannan <vikramgk007@gmail.com>
2019-09-16 02:16:05 -0400

    Add files via upload

---
+++ Lab1Solution/Lab1Solution/FirstProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class FirstProgram
+{
+    public static void Main()
+    {
+        int num;
+        do
+        {
+            num = int.Parse(Console.ReadLine());
+        } while (num==int.Parse(""));
+        Console.Write("The maximum integer you entered is " +num);
+        Console.Write("\nThe minimum integer you entered is " +num);
+        Console.Write("\nThe number of odd integer(s) you entered is " + num);
+        Console.Write("\nThe number of even integer(s) you entered is " + num);
+    }
+}
